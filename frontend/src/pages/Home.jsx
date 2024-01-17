095cc15b41a646c630c5b0b09d2dcd85b5f1132e
Baris G <barisg@pm.me>
2024-01-17 13:00:47 +0100

    📦 NEW: Debut de layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,9 @@
+import Product from "../components/Product";
+import "../styles/home.scss";
+
 function Home() {
   return (
-    <div className="w-[1440px] flex-col mx-auto">
+    <div className="max-w-[1440px] flex-col mx-auto">
       <div>TESSSSSSSSSSST</div>
       <div className="flex">
         <aside className="bg-red-400 flex-1 flex-col">
@@ -10,7 +13,13 @@ function Home() {
           <div className="">Filtre4</div>
           <div className="">Filtre5</div>
         </aside>
-        <div className="bg-green-400 flex-[3] flex">JE TEST</div>
+        <div className="bg-green-400 flex-[3] flex flex-wrap">
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+        </div>
       </div>
     </div>
   );
